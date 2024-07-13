bcedc9f78a401c4ad486c02870062d405f544efb
0fill <fikrosek@seznam.cz>
2024-07-13 12:58:27 +0200

    done prehaps
    verze 1 se kterou jsem spokojeny a na tu lambda jsem hrdy

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle, os
 import json
 
 
@@ -9,6 +9,9 @@ class Restaurant:
         self.address = address
         self.website = website
         self.ph_number = ph_number
+
+    def display_restaurant_info(self):
+        print(self.restaurant_name, self.specialization, self.address, self.website, self.ph_number)
         """try:
             data = pickle.load(open('data/restaurant.pkl', 'rb'))
         except EOFError:
@@ -30,50 +33,88 @@ class Restaurant:
 
 
 class Database:
-    def __init__(self, id):
+    def __init__(self, id: str):
+        while os.path.exists(f"data/{id}_restaurants.pkl"):
+            id = input("this id is already taken, please chose diffrent: ")
         self.__id = id
         self.path = f"data/{id}_restaurants.pkl"
-        f = open(self.path, "wb")
-        pickle.dump([], f)
-        f.close()
-
+        with open(self.path, "wb") as f:
+            pickle.dump([], f)
 
     def load(self):
         return pickle.load(open(self.path), "rb")
 
     def add(self, restaurant_name, specialization, address, website, ph_number):
-        data = Database.load()
-        pickle.dump(Restaurant(restaurant_name, specialization, address, website, ph_number),open(self.path, "wb"))
+        data = self.load()
+        data.append(Restaurant(restaurant_name, specialization, address, website, ph_number))
+        pickle.dump(data, open(self.path, "wb"))
 
-
-
-    def display(self):
-        data = pickle.load(open(self.path, "rb"))
+    def display(self):  #5
+        data = self.load()
         for r in data:
             print(f"""{r.restaurant_name}: best in {r.specialization} you can find them at {r.address} or at {r.website}
 and they deliver to home just call {r.ph_number}""")
 
     @staticmethod
-    def search(id,restaurant_name):
-        data = pickle.load(open(f"data/{id}_restaurants.pkl", "rb"))
+    def search(my_id, restaurant_name):
+        data = pickle.load(open(f"data/{my_id}_restaurants.pkl", "rb"))
         for r in data.restaurant_name:
             if restaurant_name in r:
                 if input(f"is the restaurant you're looking for {restaurant_name}?").lower() == "y":
                     return r
         return None
 
-    def remove(self):
-        restaurant_name = Restaurant.search(self.__id,input("which restaurant you want to remove: "))
-        data = pickle.load(open(self.path, "rb"))
-        data.remove(restaurant_name)
+    def remove(self):  #6
+        restaurant_name = Database.search(self.__id, input("which restaurant you want to remove: "))
+        data = self.load()
+        for r in data:
+            if r.restaurant_name == restaurant_name:
+                data.remove(r)
+        pickle.dump(data, open(self.path, "wb"))
 
+    def edit(self):
+        r_name = Database.search(self.__id, input("which restaurant you want to edit: "))
+        if r_name is None:
+            return
+        data = self.load()
+        choice = input(f"what would you like to edit?  n for name\n s for specialization\n a for address\n w for "
+                       f"website\n p for phone number")
+        for r in data:
+            if r.restaurant_name == r_name:
+                if choice == "n":
+                    r.restaurant_name = input("new restaurant name")
+                elif choice == "s":
+                    r.specialization = input("new restaurant specialization")
+                elif choice == "a":
+                    r.address = input("new restaurant address")
+                elif choice == "w":
+                    r.website = input("new restaurant website")
+                elif choice == "p":
+                    r.ph_number = input("new restaurant phone number")
+                else:
+                    return None
 
-
-
-
-
-
-
-r = Restaurant("antonio", "italiano", "adda7", "aaa.www", "7854785")
-
-#print(Restaurant.from_pickle())
+    def adv_search(self):
+        data = self.load()
+        choice = input(f"what would you like to search by?  n for name\n s for specialization\n a for address\n w for "
+                       f"website\n p for phone number")
+        if choice == "n":
+            m = lambda x: x.restaurant_name
+            s = input("which restaurant you want to search: ")
+        elif choice == 's':
+            m = lambda x: x.specialization
+            s = input("which specialization you want to search: ")
+        elif choice == "a":
+            m = lambda x: x.address
+            s = input("which address you want to search: ")
+        elif choice == "w":
+            m = lambda x: x.website
+            s = input("which website you want to search: ")
+        elif choice == "p":
+            m = lambda x: x.ph_number
+            s = input("which phone number you want to search: ")
+        else:
+            return
+        for r in data:
+            if s in m(r):
+                print(r.display_restaurant_info())
